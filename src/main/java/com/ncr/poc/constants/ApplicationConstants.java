79a69f71cdb8fcd1ab795cd8b1ce1ceed98a3651
IT Support Bomax <support@bomax.in>
2018-12-19 01:13:56 +0530

    Testing from eric

--- src/main/java/com/ncr/poc/constants/ApplicationConstants.java
+++ src/main/java/com/ncr/poc/constants/ApplicationConstants.java
@@ -2,5 +2,5 @@ package com.ncr.poc.constants;
 
 public class ApplicationConstants {
 
-	public static final int FIXED_THREAD_POOOL_SIZE = 5;
+	public static final int FIXED_THREAD_POOOL_SIZE = 10;
 }
